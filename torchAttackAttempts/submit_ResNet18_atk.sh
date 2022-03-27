653cbdf7c0c094ab9382aed5ec4c5bc39c2103b2
AlexInselmann <alexandra@bomitech.dk>
2022-03-27 13:45:11 +0200

    Make resnet and VGG ad atk ready for HPC

---
+++ torchAttackAttempts/submit_ResNet18_atk.sh
@@ -0,0 +1,35 @@
+!/bin/sh
+### General options
+### -- specify queue --
+#BSUB -q hpc
+### -- set the job Name --
+#BSUB -J Atk__ResNet18
+### -- ask for number of cores (default: 1) --
+#BSUB -n 1
+### -- specify that the cores must be on the same host --
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 2GB of memory per core/slot --
+#BSUB -R "rusage[mem=2GB]"
+### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot --
+#BSUB -M 3GB
+### -- set walltime limit: hh:mm --
+#BSUB -W 24:00
+### -- set the email address --
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+##BSUB -u s194368@student.dtu.dk
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion --
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o Output_ResNet_%J.out
+#BSUB -e Error_ResNet_%J.err
+
+# here follow the commands you want to execute
+
+#module load python3/3.8.2
+source /zhome/06/a/147115/BSc_venv/bin/activate
+
+/zhome/06/a/147115/BSc_venv/bin/python3 -u /zhome/06/a/147115/BSc_venv/BachelorProject_XAI/torchAttackAttempts/torchAttack#3_ResNet18_CIFAR100.py > /zhome/06/a/147115/BSc_venv/BachelorProject_XAI/torchAttackAttempts/outputResNet.txt
