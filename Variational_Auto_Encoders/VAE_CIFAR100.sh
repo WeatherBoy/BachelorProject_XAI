52097bcd9b2cd3444936403393f550947982cc02
AlexInselmann <alexandra@bomitech.dk>
2022-04-04 21:26:27 +0200

    Update VAE_CIFAR100.sh

--- Variational_Auto_Encoders/VAE_CIFAR100.sh
+++ Variational_Auto_Encoders/VAE_CIFAR100.sh
@@ -25,8 +25,8 @@
 #BSUB -N 
 ### -- Specify the output and error file. %J is the job-id -- 
 ### -- -o and -e mean append, -oo and -eo mean overwrite -- 
-#BSUB -o /Output_files/Output_VGG19_%J.out 
-#BSUB -e /Output_files/Error_VGG19_%J.err
+#BSUB -o Output_files/Output_VGG19_%J.out 
+#BSUB -e Output_files/Error_VGG19_%J.err
 
 # here follow the commands you want to execute 
 
